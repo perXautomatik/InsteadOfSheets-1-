4c4a5bb3347feeb490cdb902b6746e929a6a0a56
perXautomatik <christoffer.broback@gmail.com>
2019-11-21 09:44:26 +0100

    fixed connect

--- TillMinaMedelanden.sql
+++ TillMinaMedelanden.sql
@@ -1,84 +1,87 @@
-with filterBadAdress as (
-    select (SELECT master.dbo.FracToDec(andel)) 'fra',
-           FNR,
-           BETECKNING,
-           ärndenr                              'arndenr',
-           Namn,
-           Adress,
-           POSTNUMMER,
-           postOrt,
-           PERSORGNR
-    from tempExcel.dbo.InputPlusGeofir
-),
-     filterSmallOwnersBadAdress as (
-         select fra,
-                POSTORT,
-                POSTNUMMER,
-                ADRESS,
-                NAMN,
-                BETECKNING,
-                arndenr,
-                PERSORGNR,
-                RowNum
-         from (select fra,
-                      POSTORT,
-                      POSTNUMMER,
-                      ADRESS,
-                      NAMN,
-                      BETECKNING,
-                      arndenr,
-                      PERSORGNR,
-                      RowNum
-               from (select q.fra,
-                            q.POSTORT,
-                            q.POSTNUMMER,
-                            q.ADRESS,
-                            q.NAMN,
-                            q.BETECKNING,
-                            q.arndenr,
-                            q.PERSORGNR,
-                            ROW_NUMBER() OVER ( PARTITION BY q.arndenr ORDER BY q.fra desc) RowNum
-                     from filterBadAdress as q
-                              INNER JOIN filterBadAdress thethree
-                                         ON q.arndenr = thethree.arndenr and q.namn = thethree.namn) X
-               WHERE X.RowNum = 1 AND postOrt <> '' AND POSTNUMMER <> '' AND Adress <> '' AND Namn is not null) as asdasd
-         union
-         select fra,
-                POSTORT,
-                POSTNUMMER,
-                ADRESS,
-                NAMN,
-                BETECKNING,
-                arndenr,
-                PERSORGNR,
-                RowNum
-         from (select fra,
-                      POSTORT,
-                      POSTNUMMER,
-                      ADRESS,
-                      NAMN,
-                      BETECKNING,
-                      arndenr,
-                      PERSORGNR,
-                      RowNum
-               from (select q.fra,
-                            q.POSTORT,
-                            q.POSTNUMMER,
-                            q.ADRESS,
-                            q.NAMN,
-                            q.BETECKNING,
-                            q.arndenr,
-                            q.PERSORGNR,
-                            ROW_NUMBER() OVER ( PARTITION BY q.arndenr ORDER BY q.fra desc ) RowNum
-                     from filterBadAdress as q
-                              INNER JOIN filterBadAdress thethree
-                                         ON q.arndenr = thethree.arndenr and q.namn = thethree.namn) X
-               WHERE X.RowNum > 1
-                 and X.RowNum < 4
-                 AND fra > 0.3
-                 AND postOrt <> '' AND POSTNUMMER <> '' AND Adress <> '' AND Namn is not null) as asdasdx)
-        ,
-     adressCompl as (select fra,
+WITH filterBadAdress AS (SELECT (SELECT master.dbo.FracToDec(andel)) 'fra',
+                                FNR,
+                                BETECKNING,
+                                ärndenr                              'arndenr',
+                                Namn,
+                                Adress,
+                                POSTNUMMER,
+                                postOrt,
+                                PERSORGNR
+                         FROM tempExcel.dbo.InputPlusGeofir),
+     filterSmallOwnersBadAdress AS (SELECT fra,
+                                           POSTORT,
+                                           POSTNUMMER,
+                                           ADRESS,
+                                           NAMN,
+                                           BETECKNING,
+                                           arndenr,
+                                           PERSORGNR,
+                                           RowNum
+                                    FROM (SELECT fra,
+                                                 POSTORT,
+                                                 POSTNUMMER,
+                                                 ADRESS,
+                                                 NAMN,
+                                                 BETECKNING,
+                                                 arndenr,
+                                                 PERSORGNR,
+                                                 RowNum
+                                          FROM (SELECT q.fra,
+                                                       q.POSTORT,
+                                                       q.POSTNUMMER,
+                                                       q.ADRESS,
+                                                       q.NAMN,
+                                                       q.BETECKNING,
+                                                       q.arndenr,
+                                                       q.PERSORGNR,
+                                                       ROW_NUMBER() OVER (PARTITION BY q.arndenr ORDER BY q.fra DESC) RowNum
+                                                FROM filterBadAdress AS q
+                                                         INNER JOIN filterBadAdress thethree
+                                                                    ON q.arndenr = thethree.arndenr AND q.namn = thethree.namn) X
+                                          WHERE X.RowNum = 1
+                                            AND postOrt <> ''
+                                            AND POSTNUMMER <> ''
+                                            AND Adress <> ''
+                                            AND Namn IS NOT NULL) AS asdasd
+                                    UNION
+                                    SELECT fra,
+                                           POSTORT,
+                                           POSTNUMMER,
+                                           ADRESS,
+                                           NAMN,
+                                           BETECKNING,
+                                           arndenr,
+                                           PERSORGNR,
+                                           RowNum
+                                    FROM (SELECT fra,
+                                                 POSTORT,
+                                                 POSTNUMMER,
+                                                 ADRESS,
+                                                 NAMN,
+                                                 BETECKNING,
+                                                 arndenr,
+                                                 PERSORGNR,
+                                                 RowNum
+                                          FROM (SELECT q.fra,
+                                                       q.POSTORT,
+                                                       q.POSTNUMMER,
+                                                       q.ADRESS,
+                                                       q.NAMN,
+                                                       q.BETECKNING,
+                                                       q.arndenr,
+                                                       q.PERSORGNR,
+                                                       ROW_NUMBER() OVER (PARTITION BY q.arndenr ORDER BY q.fra DESC) RowNum
+                                                FROM filterBadAdress AS q
+                                                         INNER JOIN filterBadAdress thethree
+                                                                    ON q.arndenr = thethree.arndenr AND q.namn = thethree.namn) X
+                                          WHERE X.RowNum > 1
+                                            AND X.RowNum < 4
+                                            AND fra > 0.3
+                                            AND postOrt <> ''
+                                            AND POSTNUMMER <> ''
+                                            AND Adress <> ''
+                                            AND Namn IS NOT NULL) AS asdasdx),
+     adressCompl AS (SELECT fra,
                             AdressComplettering.POSTORT,
                             AdressComplettering.POSTNUMMER,
                             AdressComplettering.ADRESS,
@@ -87,7 +90,7 @@ with filterBadAdress as (
                             toComplete.arndenr,
                             PERSORGNR,
                             RowNum
-                     from (select fra,
+                     FROM (SELECT fra,
                                   POSTORT,
                                   POSTNUMMER,
                                   ADRESS,
@@ -96,17 +99,34 @@ with filterBadAdress as (
                                   arndenr,
                                   PERSORGNR,
                                   RowNum
-                           from filterSmallOwnersBadAdress
-                           where postOrt = '' OR POSTNUMMER = '' OR Adress = '' OR Namn is null) as toComplete
-                              left outer join tempExcel.dbo.AdressComplettering
-                                              on AdressComplettering.arndenr = toComplete.arndenr)
-select *
-from adressCompl
-union
-(select *
-from filterSmallOwnersBadAdress
-where postOrt <> '' AND POSTNUMMER <> '' AND Adress <> '' AND Namn is not null)
-order by arndenr, RowNum
-
-
-
+                           FROM filterSmallOwnersBadAdress
+                           WHERE postOrt = ''
+                              OR POSTNUMMER = ''
+                              OR Adress = ''
+                              OR Namn IS NULL) AS toComplete
+                              LEFT OUTER JOIN tempExcel.dbo.AdressComplettering
+                                              ON AdressComplettering.arndenr = toComplete.arndenr)
+SELECT DISTINCT adressCompl.fra,
+                adressCompl.POSTORT,
+                adressCompl.POSTNUMMER,
+                adressCompl.ADRESS,
+                adressCompl.NAMN,
+                adressCompl.BETECKNING,
+                adressCompl.arndenr,
+                adressCompl.PERSORGNR
+FROM adressCompl
+UNION
+(SELECT filterSmallOwnersBadAdress.fra,
+        filterSmallOwnersBadAdress.POSTORT,
+        filterSmallOwnersBadAdress.POSTNUMMER,
+        filterSmallOwnersBadAdress.ADRESS,
+        filterSmallOwnersBadAdress.NAMN,
+        filterSmallOwnersBadAdress.BETECKNING,
+        filterSmallOwnersBadAdress.arndenr,
+        filterSmallOwnersBadAdress.PERSORGNR
+ FROM filterSmallOwnersBadAdress
+ WHERE postOrt <> ''
+   AND POSTNUMMER <> ''
+   AND Adress <> ''
+   AND Namn IS NOT NULL)
+ORDER BY arndenr;
--- newConsept.sql
+++ newConsept.sql
@@ -1,4 +1,13 @@
-first find top 3 owners then
+--first find top 3 owners then
 
-stuff fastighetsbeteckning
-stuff namn med samma fastigheter
+--stuff fastighetsbeteckning
+--stuff namn med samma fastigheter
+
+--STUFF( (   SELECT ',' + CONVERT(NVARCHAR(20), StudentId) FROM Student WHERE condition = abc FOR xml path('') ) , 1 , 1 , '')
+
+--f the StudentId is coming from a non varchar column, you will probably get a conversion exception. You need to use the CONCAT function to do proper concatenation
+
+SELECT StudentID = COALESCE(StudentID + ',', '') + StudentID
+FROM Student
+WHERE StudentID IS NOT NULL
+  and Condition = 'XYZ'
