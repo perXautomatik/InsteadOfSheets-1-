b85f3156e8020c5f081d68469ab401c5fa94c5c6
perXautomatik <christoffer.broback@gmail.com>
2019-11-21 12:23:16 +0100

    Some Code Rearanging

--- TillMinaMedelanden.sql
+++ TillMinaMedelanden.sql
@@ -1,27 +1,28 @@
 WITH
+     FracToDec AS (SELECT (SELECT master.dbo.FracToDec(andel)) 'fra',FNR,BETECKNING,ärndenr 'arndenr',Namn,Adress,POSTNUMMER,postOrt,PERSORGNR FROM tempExcel.dbo.InputPlusGeofir),
+
+     RowNrByBeteckning as (SELECT q.fra,q.POSTORT,q.POSTNUMMER,q.ADRESS,q.NAMN,q.BETECKNING,q.arndenr,q.PERSORGNR,
+                                         ROW_NUMBER() OVER (PARTITION BY q.BETECKNING ORDER BY q.fra DESC) RowNum
+                                  FROM FracToDec AS q
+                                           INNER JOIN FracToDec thethree
+                                                      ON q.arndenr = thethree.arndenr AND q.namn = thethree.namn),
+
+     giveRowNrFilterBad as (SELECT fra,POSTORT,POSTNUMMER,ADRESS,NAMN,BETECKNING,arndenr,PERSORGNR,RowNum
+                            FROM RowNrByBeteckning
+                            WHERE RowNrByBeteckning.postOrt <> ''
+                              AND RowNrByBeteckning.POSTNUMMER <> ''
+                              AND RowNrByBeteckning.Adress <> ''
+                              AND RowNrByBeteckning.Namn IS NOT NULL),
 
 
-     filterBadAdress AS (SELECT (SELECT master.dbo.FracToDec(andel)) 'fra',FNR,BETECKNING,ärndenr 'arndenr',Namn,Adress,POSTNUMMER,postOrt,PERSORGNR FROM tempExcel.dbo.InputPlusGeofir),
-
-     X as (SELECT fra,POSTORT,POSTNUMMER,ADRESS,NAMN,BETECKNING,arndenr,PERSORGNR,RowNum FROM (SELECT q.fra,q.POSTORT,q.POSTNUMMER,q.ADRESS,q.NAMN,q.BETECKNING,q.arndenr,q.PERSORGNR,ROW_NUMBER() OVER (PARTITION BY q.arndenr ORDER BY q.fra DESC) RowNum FROM filterBadAdress AS q INNER JOIN filterBadAdress thethree ON q.arndenr = thethree.arndenr AND q.namn = thethree.namn) as Xtemp WHERE Xtemp.postOrt <> '' AND Xtemp.POSTNUMMER <> '' AND Xtemp.Adress <> '' AND Xtemp.Namn IS NOT NULL),
-
-
-     filterSmallOwnersBadAdress AS (select * from X where  X.RowNum = 1
+     filterSmallOwnersBadAdress AS (select * from giveRowNrFilterBad where  giveRowNrFilterBad.RowNum = 1
                                     UNION
-                                    select * from  X where X.RowNum > 1 AND X.RowNum < 4 AND fra > 0.3 ),
+                                    select * from  giveRowNrFilterBad where giveRowNrFilterBad.RowNum > 1 AND giveRowNrFilterBad.RowNum < 4 AND fra > 0.3 ),
 
 
-     adressCompl AS (SELECT fra,
-                            AdressComplettering.POSTORT,AdressComplettering.POSTNUMMER,AdressComplettering.ADRESS,AdressComplettering.NAMN,BETECKNING,toComplete.arndenr,PERSORGNR,RowNum
-                     FROM (SELECT fra,
-                                  POSTORT,POSTNUMMER,ADRESS,NAMN,BETECKNING,arndenr,PERSORGNR,RowNum
-                           FROM filterSmallOwnersBadAdress
-                           WHERE postOrt = ''
-                              OR POSTNUMMER = ''
-                              OR Adress = ''
-                              OR Namn IS NULL) AS toComplete
-                              LEFT OUTER JOIN tempExcel.dbo.AdressComplettering
-                                              ON AdressComplettering.arndenr = toComplete.arndenr)
+     adressCompl AS (SELECT fra,AdressComplettering.POSTORT,AdressComplettering.POSTNUMMER,AdressComplettering.ADRESS,AdressComplettering.NAMN,BETECKNING,toComplete.arndenr,PERSORGNR,RowNum
+                     FROM (SELECT fra,POSTORT,POSTNUMMER,ADRESS,NAMN,BETECKNING,arndenr,PERSORGNR,RowNum
+                           FROM RowNrByBeteckning WHERE postOrt = '' OR POSTNUMMER = '' OR Adress = '' OR Namn IS NULL) AS toComplete LEFT OUTER JOIN tempExcel.dbo.AdressComplettering ON AdressComplettering.arndenr = toComplete.arndenr)
 
 
 
@@ -29,4 +30,4 @@ WITH
 
 --SELECT DISTINCT adressCompl.fra,adressCompl.POSTORT,adressCompl.POSTNUMMER,adressCompl.ADRESS,adressCompl.NAMN,adressCompl.BETECKNING,adressCompl.arndenr,adressCompl.PERSORGNR FROM adressCompl UNION(SELECT filterSmallOwnersBadAdress.fra,filterSmallOwnersBadAdress.POSTORT,filterSmallOwnersBadAdress.POSTNUMMER,filterSmallOwnersBadAdress.ADRESS,filterSmallOwnersBadAdress.NAMN,filterSmallOwnersBadAdress.BETECKNING,filterSmallOwnersBadAdress.arndenr,filterSmallOwnersBadAdress.PERSORGNR FROM filterSmallOwnersBadAdress WHERE postOrt <> '' AND POSTNUMMER <> '' AND Adress <> '' AND Namn IS NOT NULL)ORDER BY arndenr;
 
-select * from X
+select * from filterSmallOwnersBadAdress
