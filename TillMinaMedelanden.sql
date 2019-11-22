962bd74f118f27fd7159a7235f32a6894265d29d
perXautomatik <christoffer.broback@gmail.com>
2019-11-22 10:49:03 +0100

    successfully stuff

--- TillMinaMedelanden.sql
+++ TillMinaMedelanden.sql
@@ -1,9 +1,30 @@
-   IF OBJECT_ID('tempdb..#FracToDec6','U') IS NOT NULL
-       DROP TABLE FracToDec6
+   IF OBJECT_ID('master.dbo.FracToDec8','U') IS NOT NULL
+       DROP TABLE FracToDec8
 
-   create table dbo.FracToDec6
+   create table dbo.FracToDec8
     (
-  fra decimal,
+  fra decimal(14,6),
+  POSTORT varchar(255),
+  POSTNUMMER varchar(255),
+  ADRESS varchar(255),
+  NAMN varchar(255),
+  BETECKNING nvarchar(255),
+  arndenr nvarchar(255),
+  PERSORGNR nvarchar(255)
+);
+
+--SET IDENTITY_INSERT FracToDec3 ON;
+
+INSERT INTO dbo.FracToDec8(fra, POSTORT, POSTNUMMER, ADRESS, NAMN, BETECKNING, arndenr, PERSORGNR)
+        SELECT distinct (SELECT master.dbo.FracToDec(andel)) 'fra',POSTORT, POSTNUMMER, ADRESS, NAMN, BETECKNING, tempExcel.dbo.InputPlusGeofir.ärndenr, PERSORGNR FROM tempExcel.dbo.InputPlusGeofir;
+
+
+   IF OBJECT_ID('master.dbo.RowNrByBeteckning','U') IS NOT NULL
+       DROP TABLE RowNrByBeteckning
+
+   create table dbo.RowNrByBeteckning
+    (
+  fra decimal(14,6),
   POSTORT varchar(255),
   POSTNUMMER varchar(255),
   ADRESS varchar(255),
@@ -14,22 +35,12 @@
   RowNum bigint
 );
 
---SET IDENTITY_INSERT FracToDec3 ON;
-
-INSERT INTO dbo.FracToDec6(fra, POSTORT, POSTNUMMER, ADRESS, NAMN, BETECKNING, arndenr, PERSORGNR, RowNum)
-        SELECT distinct (SELECT master.dbo.FracToDec(andel)) 'fra',FNR,BETECKNING,ärndenr 'arndenr',Namn,Adress,POSTNUMMER,postOrt,PERSORGNR FROM tempExcel.dbo.InputPlusGeofir;
-
-
+INSERT INTO dbo.RowNrByBeteckning(fra, POSTORT, POSTNUMMER, ADRESS, NAMN, BETECKNING, arndenr, PERSORGNR,RowNum)
+		SELECT distinct q.fra,q.POSTORT,q.POSTNUMMER,q.ADRESS,q.NAMN,q.BETECKNING,q.arndenr,q.PERSORGNR,
+                                         ROW_NUMBER() OVER (PARTITION BY q.BETECKNING ORDER BY q.fra DESC) RowNum
+                                  FROM FracToDec8 AS q INNER JOIN FracToDec8 thethree ON q.BETECKNING = thethree.BETECKNING AND q.namn = thethree.namn;
 
 WITH
-
-
-
-
-     RowNrByBeteckning as (SELECT distinct q.fra,q.POSTORT,q.POSTNUMMER,q.ADRESS,q.NAMN,q.BETECKNING,q.arndenr,q.PERSORGNR,
-                                         ROW_NUMBER() OVER (PARTITION BY q.BETECKNING ORDER BY q.fra DESC) RowNum
-                                  FROM FracToDec6 AS q INNER JOIN FracToDec6 thethree ON q.BETECKNING = thethree.BETECKNING AND q.namn = thethree.namn),
-
      FilterBad as (SELECT fra,POSTORT,POSTNUMMER,ADRESS,NAMN,BETECKNING,arndenr,PERSORGNR,RowNum
                             FROM RowNrByBeteckning
                             WHERE RowNrByBeteckning.postOrt <> ''
@@ -43,14 +54,12 @@ WITH
                                     select * from  FilterBad where FilterBad.RowNum > 1 AND FilterBad.RowNum < 4 AND fra > 0.3 )
 
 
-     --,adressCompl AS (SELECT fra,AdressComplettering.POSTORT,AdressComplettering.POSTNUMMER,AdressComplettering.ADRESS,AdressComplettering.NAMN,BETECKNING,toComplete.arndenr,PERSORGNR,RowNum FROM (SELECT fra,POSTORT,POSTNUMMER,ADRESS,NAMN,BETECKNING,arndenr,PERSORGNR,RowNum FROM RowNrByBeteckning WHERE postOrt = '' OR POSTNUMMER = '' OR Adress = '' OR Namn IS NULL) AS toComplete LEFT OUTER JOIN tempExcel.dbo.AdressComplettering ON AdressComplettering.arndenr = toComplete.arndenr)
-
-
-
-
-                --,old as ( SELECT DISTINCT adressCompl.fra,adressCompl.POSTORT,adressCompl.POSTNUMMER,adressCompl.ADRESS,adressCompl.NAMN,adressCompl.BETECKNING,adressCompl.arndenr,adressCompl.PERSORGNR FROM adressCompl UNION(SELECT filterSmallOwnersBadAdress.fra,filterSmallOwnersBadAdress.POSTORT,filterSmallOwnersBadAdress.POSTNUMMER,filterSmallOwnersBadAdress.ADRESS,filterSmallOwnersBadAdress.NAMN,filterSmallOwnersBadAdress.BETECKNING,filterSmallOwnersBadAdress.arndenr,filterSmallOwnersBadAdress.PERSORGNR FROM filterSmallOwnersBadAdress WHERE postOrt <> '' AND POSTNUMMER <> '' AND Adress <> '' AND Namn IS NOT NULL)ORDER BY arndenr)
-
--- select POSTORT,POSTNUMMER,ADRESS,NAMN,STUFF((SELECT ',' + CAST(innerTable.BETECKNING AS nvarchar(50))FROM filterSmallOwners AS innerTable WHERE innerTable.Namn = ressult.Namn FOR XML PATH('')),1,1,'') AS Ids from filterSmallOwners ressult group by namn, POSTORT, POSTNUMMER, ADRESS
-
-
-select * from filterSmallOwners
+	,stuffBeteckning as ( select arndenr,POSTORT,POSTNUMMER,ADRESS,NAMN,STUFF((SELECT ', ' + CAST(innerTable.BETECKNING AS nvarchar(50)) FROM filterSmallOwners AS innerTable WHERE innerTable.Namn = ressult.Namn order by Beteckning FOR XML PATH('')),1,1,'') AS Beteckning from filterSmallOwners ressult group by namn, POSTORT, POSTNUMMER, ADRESS,arndenr)
+	
+	,stuffNamn as ( select arndenr,POSTORT,POSTNUMMER,ADRESS,STUFF((SELECT ', ' + CAST(innerTable.Namn AS nvarchar(50)) FROM stuffBeteckning AS innerTable WHERE innerTable.Beteckning = ressult.Beteckning AND innerTable.ADRESS = ressult.ADRESS order by Beteckning FOR XML PATH('')),1,1,'') AS NAMN,Beteckning from stuffBeteckning ressult group by Beteckning, POSTORT, POSTNUMMER, ADRESS,arndenr)
+   
+   
+ select * from stuffNamn order by Beteckning
+   
+   IF OBJECT_ID('master..#FracToDec8','U') IS NOT NULL
+       DROP TABLE FracToDec8
